03b81c888324a7df094bdb9a12e1decfea93b9f7
Oberemko Viktor <oberemkovictor@gmail.com>
2016-09-29 18:21:42 +0300

    started to do drag&drop for elements

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,8 @@ VK.init({
   apiId: 5629026
 });
 
+let users;
+
 new Promise((resolve, reject) => {
   VK.Auth.login(function(response) {
     if(response.session) {
@@ -23,16 +25,33 @@ new Promise((resolve, reject) => {
   });
 }).then((answer) => {
   if(answer) {
+    window.users = answer;
+    console.log(users);
     createUserBlock(answer);
+
+    var firstFriendInput = document.querySelector(".firstFriendInput");
+    firstFriendInput.addEventListener('keyup', (event) => {
+      var searchFriend = firstFriendInput.value;
+      var newArrayOfUsers = filterUsers(users, searchFriend.toLowerCase());
+
+      createUserBlock(newArrayOfUsers);
+    });
   }
 })
 
 function createUserBlock(users) {
   var usersBlock = document.querySelector('.filter-app-friends-your');
+  usersBlock.innerHTML = "";
   var docFragment = document.createDocumentFragment();
   users.forEach((user) => {
     let div = document.createElement('div');
     div.classList.add('item');
+    if(!div.draggable) {
+      div.setAttribute("draggable", "true");
+      div.addEventListener('drag', (event) => {
+        console.log(event);
+      })
+    }
     div.innerHTML = `
                       <div class="item-logo" style="background: url(${user.photo_100}); background-size: contain;"></div>
                       <h3 class="item-title">${user.first_name} ${user.last_name}</h3>
@@ -44,3 +63,20 @@ function createUserBlock(users) {
   })
   usersBlock.appendChild(docFragment);
 }
+
+function filterUsers(array, value) {
+  let filteredArray = [];
+
+  array.forEach((user) => {
+    if(user.first_name.toLowerCase().indexOf(value) != -1) {
+      filteredArray.push(user);
+    }
+  });
+
+  return filteredArray;
+}
+
+var friendsInList = document.querySelector(".filter-app-friends-in-list");
+firstFriendInput.addEventListener('drop', (event) => {
+  event.preventDefault();
+})
--- app/js/build.js
+++ app/js/build.js
@@ -4,6 +4,8 @@ VK.init({
   apiId: 5629026
 });
 
+var users = void 0;
+
 new Promise(function (resolve, reject) {
   VK.Auth.login(function (response) {
     if (response.session) {
@@ -23,19 +25,53 @@ new Promise(function (resolve, reject) {
   });
 }).then(function (answer) {
   if (answer) {
+    window.users = answer;
+    console.log(users);
     createUserBlock(answer);
+
+    var firstFriendInput = document.querySelector(".firstFriendInput");
+    firstFriendInput.addEventListener('keyup', function (event) {
+      var searchFriend = firstFriendInput.value;
+      var newArrayOfUsers = filterUsers(users, searchFriend.toLowerCase());
+
+      createUserBlock(newArrayOfUsers);
+    });
   }
 });
 
 function createUserBlock(users) {
   var usersBlock = document.querySelector('.filter-app-friends-your');
+  usersBlock.innerHTML = "";
   var docFragment = document.createDocumentFragment();
   users.forEach(function (user) {
     var div = document.createElement('div');
     div.classList.add('item');
+    if (!div.draggable) {
+      div.setAttribute("draggable", "true");
+      div.addEventListener('drag', function (event) {
+        console.log(event);
+      });
+    }
     div.innerHTML = '\n                      <div class="item-logo" style="background: url(' + user.photo_100 + '); background-size: contain;"></div>\n                      <h3 class="item-title">' + user.first_name + ' ' + user.last_name + '</h3>\n                      <div class="close item-close">\n                        <i class="fa fa-plus" aria-hidden="true"></i>\n                      </div>\n                    ';
     docFragment.appendChild(div);
   });
   usersBlock.appendChild(docFragment);
 }
+
+function filterUsers(array, value) {
+  var filteredArray = [];
+
+  array.forEach(function (user) {
+    if (user.first_name.toLowerCase().indexOf(value) != -1) {
+      filteredArray.push(user);
+    }
+  });
+
+  return filteredArray;
+}
+
+var friendsInList = document.querySelector(".filter-app-friends-in-list");
+firstFriendInput.addEventListener('drop', function (event) {
+  event.preventDefault();
+});
 //# sourceMappingURL=build.js.map
