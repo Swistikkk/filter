b32dbd133244a84a584ee50608db93e48e82e0d8
Viktor <oberemkovictor@gmail.com>
2017-02-06 00:05:51 +0300

    Try to use webpack

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,40 @@
-let allUsers;
-
-window.addEventListener('load', (event) => {
-    VK.init({
-        apiId: 5629026,
-    });
-
-    return new Promise((resolve, reject) => {
-        VK.Auth.login(function(response){
-            if(response.status == 'connected') {
-                resolve();
-                console.log(response);
-            } else {
-                console.log(response);
-            }
-        }, 2 | 4);
-    }).then(()=> {
-        VK.api('friends.get',{fields: 'photo_100', order: 'name', name_case: 'nom'},function(answer) {
-            allUsers = answer.response;
-            console.log(answer);
-        });
-    });
+VK.init({
+  apiId: 5629026,
 });
+
+new Promise((resolve, reject) => {
+  VK.Auth.login(function(response){
+    if(response.status == 'connected') {
+      resolve();
+    } else {
+      console.log(response);
+    }
+  }, 2 | 4);
+}).then(()=> {
+  return new Promise((resolve) => {
+    VK.api('friends.get',{fields: 'photo_100', order: 'name', name_case: 'nom'}, (answer) => {
+      if(answer.response) {
+        resolve(answer.response);
+      }
+    });
+  })
+}).then((vkUsers) => {
+  var vkFriendsBlock = document.querySelector('.filter-app-friends-your');
+  console.log(vkUsers);
+  vkUsers.forEach((user) => {
+    let userItem = document.createElement('div');
+    userItem.classList.add('item');
+    userItem.innerHTML = `
+        <div class="item-logo" style="background: url(${user.photo_100}); background-size: contain; "></div>
+        <h3 class="item-title">${user.first_name} ${user.last_name}</h3>
+        <div class="close item-close">
+          <i class="fa fa-plus" aria-hidden="true"></i>
+        </div>`
+    vkFriendsBlock.appendChild(userItem);
+  })
+}).then(() => {
+  let userVkFilterInput = document.querySelector('.filter-app-search-input');
+  userVkFilterInput.addEventListener('keyup', (event) => {
+
+  })
+})
