fbf7f68b828a56fd09e08e3c8302c535107210bf
Victor Oberemko <oberemkovictor@gmail.com>
2017-02-06 11:36:19 +0300

    Last version of my code

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,27 @@
-let allUsers;
-
-window.addEventListener('load', (event) => {
+function getUsers() {
     VK.init({
         apiId: 5629026,
     });
 
-    return new Promise((resolve, reject) => {
+    new Promise((resolve, reject) => {
         VK.Auth.login(function(response){
             if(response.status == 'connected') {
                 resolve();
-                console.log(response);
             } else {
                 console.log(response);
             }
         }, 2 | 4);
     }).then(()=> {
         VK.api('friends.get',{fields: 'photo_100', order: 'name', name_case: 'nom'},function(answer) {
-            allUsers = answer.response;
-            console.log(answer);
+            return answer.response;
         });
-    });
+    })
+}
+
+window.addEventListener('load', (event) => {
+
 });
+
+let allUsers = getUsers();
+
+console.log(allUsers);
