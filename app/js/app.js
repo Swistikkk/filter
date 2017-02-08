76c8ab1baf2ac2f209856e6d94314f350bd3b9c4
Victor Oberemko <oberemkovictor@gmail.com>
2017-02-08 15:20:56 +0300

    Refactoring code

--- app/js/app.js
+++ app/js/app.js
@@ -2,85 +2,60 @@ VK.init({
   apiId: 5629026,
 });
 
-new Promise((resolve, reject) => {
-  VK.Auth.login(function(response){
-    if(response.status == 'connected') {
-      resolve();
-    } else {
-      console.log(response);
-    }
-  }, 2 | 4);
-}).then(()=> {
-  return new Promise((resolve) => {
-    VK.api('friends.get',{fields: 'photo_100', order: 'name', name_case: 'nom'}, (answer) => {
-      if(answer.response) {
-        resolve(answer.response);
-      }
-    });
-  })
-}).then((usersVk) => {
-  let vkUsers = getFromLocalStorage('vkUsers') || usersVk;
-  let myFriends = getFromLocalStorage('myFriends') || [];
+function getFromLocalStorage(key) {
+  return JSON.parse(localStorage[key] || null);
+};
+
+let vkUsers = getFromLocalStorage('vkUsers') || null;
+let myFriends = getFromLocalStorage('myFriends') || [];
+
+window.addEventListener('load', () => {
   let searchInputMyFriend = document.querySelector('.filter-app-search-input__myFriends input');
   let vkFriendsBlock = document.querySelector('.filter-app-friends-your__wrapper');
   let myFriendBlock = document.querySelector('.filter-app-friends-in-list__wrapper');
   let searchInputVkFriends = document.querySelector('.filter-app-search-input input');
   let saveButton = document.querySelector('.filter-app-footer button');
 
-  function getFromLocalStorage(key) {
-    if(localStorage[key]) {
-      return JSON.parse(localStorage[key]);
-    }
-
-    return false;
-  };
-
-  function renderUsers(arrayOfUsers, blockToAppend, action) {
-    blockToAppend.innerHTML = '';
-
-    arrayOfUsers.forEach((user) => {
-      let userItem = document.createElement('div');
-
-      userItem.setAttribute('draggable', true);
-      userItem.classList.add('item');
-      userItem.setAttribute('data-id', user.user_id);
-      userItem.setAttribute('id', user.user_id);
-
-      if(action == 'remove') {
-        userItem.innerHTML = `
-        <div class="item-logo" style="background: url(${user.photo_100}); background-size: contain; "></div>
-        <h3 class="item-title">${user.first_name} ${user.last_name}</h3>
-        <div class="close item-close">
-          <i class="fa fa-times" aria-hidden="true"></i>
-        </div>`;
-      } else {
-        userItem.innerHTML = `
-        <div class="item-logo" style="background: url(${user.photo_100}); background-size: contain; "></div>
-        <h3 class="item-title">${user.first_name} ${user.last_name}</h3>
-        <div class="close item-close">
-          <i class="fa fa-plus" aria-hidden="true"></i>
-        </div>`;
-      }
-
-      userItem.addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData("userItem", event.target.dataset.id);
-      });
-
-      blockToAppend.appendChild(userItem);
-    });
+  if(!vkUsers) {
+    new Promise((resolve, reject) => {
+      VK.Auth.login(function(response){
+        if(response.status == 'connected') {
+          resolve();
+        } else {
+          console.log(response);
+        }
+      }, 2 | 4);
+    }).then(()=> {
+      return new Promise((resolve) => {
+        VK.api('friends.get',{fields: 'photo_100', order: 'name', name_case: 'nom'}, (answer) => {
+          if(answer.response) {
+            vkUsers = answer.response;
+            console.log(vkUsers);
+            resolve();
+          }
+        });
+      })
+    }).then(() => {
+      initFilterApp(vkFriendsBlock, myFriendBlock);
+    })
+  } else {
+    initFilterApp(vkFriendsBlock, myFriendBlock);
   }
 
-  renderUsers(vkUsers, vkFriendsBlock);
-  renderUsers(myFriends, myFriendBlock, 'remove');
+
+  vkFriendsBlock.addEventListener('click', (event) => {
+    actionOnClick(event, 'vk', vkFriendsBlock, myFriendBlock);
+  });
+
+  myFriendBlock.addEventListener('click', (event) => {
+    actionOnClick(event, 'myFriend', vkFriendsBlock, myFriendBlock);
+  });
+
 
   searchInputVkFriends.addEventListener('keyup', (event) => {
     let inputValue = event.currentTarget.value;
     let searchArrayOfUsers = vkUsers.filter((user) => {
-      if(user.first_name.indexOf(inputValue) != -1) {
-        return true;
-      } else {
-        return false;
-      }
+      return user.first_name.includes(inputValue) == true;
     });
 
     renderUsers(searchArrayOfUsers, vkFriendsBlock);
@@ -89,74 +64,14 @@ new Promise((resolve, reject) => {
   searchInputMyFriend.addEventListener('keyup', (event) => {
     let inputValue = event.currentTarget.value;
     let searchArrayOfUsers = myFriends.filter((user) => {
-      if(user.first_name.indexOf(inputValue) != -1) {
-        return true;
-      } else {
-        return false;
-      }
+      return user.first_name.includes(inputValue) == true;
     });
 
     renderUsers(searchArrayOfUsers, myFriendBlock, 'remove');
   });
 
-  vkFriendsBlock.addEventListener('click', (event) => {
-    if(event.target.classList.contains('item-close') || event.target.classList.contains('fa-plus')) {
-      // ToDO Сделать проверку если это крести и если это блок крестика
-      let idUser = event.target.parentElement.parentElement.dataset.id;
 
-      setVkUsers(idUser);
-
-      renderUsers(vkUsers, vkFriendsBlock);
-      renderUsers(myFriends, myFriendBlock, 'remove');
-    }
-  });
-
-  myFriendBlock.addEventListener('click', (event) => {
-    if(event.target.classList.contains('item-close') || event.target.classList.contains('fa-times')) {
-      // ToDO Сделать проверку если это крести и если это блок крестика
-      let idUser = event.target.parentElement.parentElement.dataset.id;
-
-      setMyFriends(idUser);
-      renderUsers(vkUsers, vkFriendsBlock);
-      renderUsers(myFriends, myFriendBlock, 'remove');
-    }
-  });
-
-  function setVkUsers(id) {
-    vkUsers = vkUsers.filter((user) => {
-      if(user.user_id == id) {
-        myFriends.push(user);
-      }
-
-      if(user.user_id != id) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
-
-  function setMyFriends(id) {
-    myFriends = myFriends.filter((user) => {
-      if(user.user_id == id) {
-        vkUsers.unshift(user);
-      }
-
-      if(user.user_id !=id) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-
-  }
-
-
-
-
-
-  // Drop to vkFriendBlock
-
+  // Drag & Drop HTML5 Api
   vkFriendsBlock.addEventListener('dragover', (event) => {
     event.preventDefault();
   });
@@ -171,13 +86,6 @@ new Promise((resolve, reject) => {
     renderUsers(vkUsers, vkFriendsBlock);
   });
 
-
-
-
-
-
-  // Drop to myFriendsBlock
-
   myFriendBlock.addEventListener('dragover', (event) => {
     event.preventDefault();
   });
@@ -190,12 +98,92 @@ new Promise((resolve, reject) => {
 
     setVkUsers(data);
     renderUsers(myFriends, myFriendBlock, 'remove');
-  })
+  });
 
-
+  // Save button to localStorage
   saveButton.addEventListener('click', (event) => {
     localStorage.setItem('vkUsers', JSON.stringify(vkUsers));
     localStorage.setItem('myFriends', JSON.stringify(myFriends));
   });
 });
+
+
+function initFilterApp(vkFriendsBlock, myFriendBlock,) {
+  renderUsers(vkUsers, vkFriendsBlock);
+  renderUsers(myFriends, myFriendBlock, 'remove');
+}
+
+function renderUsers(arrayOfUsers, blockToAppend, action) {
+  blockToAppend.innerHTML = '';
+
+  arrayOfUsers.forEach((user) => {
+    let userItem = document.createElement('div');
+    let innerNodes = `<div class="item-logo" style="background: url(${user.photo_100}); background-size: contain; "></div>
+                      <h3 class="item-title">${user.first_name} ${user.last_name}</h3>`;
+
+    userItem.setAttribute('draggable', true);
+    userItem.classList.add('item');
+    userItem.setAttribute('data-id', user.user_id);
+    userItem.setAttribute('id', user.user_id);
+
+    if(action == 'remove') {
+      innerNodes = innerNodes.concat(`
+        <div class="close item-close">
+           <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+      `);
+    } else {
+      innerNodes = innerNodes.concat(`
+        <div class="close item-close">
+           <i class="fa fa-plus" aria-hidden="true"></i>
+        </div>
+      `);
+    };
+
+    userItem.innerHTML = innerNodes;
+
+    userItem.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData("userItem", event.target.dataset.id);
+    });
+
+    blockToAppend.appendChild(userItem);
+  });
+};
+
+function setVkUsers(id) {
+  vkUsers = vkUsers.filter((user) => {
+    if(user.user_id == id) {
+      myFriends.push(user);
+    }
+
+    return user.user_id != id
+  });
+};
+
+function setMyFriends(id) {
+  myFriends = myFriends.filter((user) => {
+    if(user.user_id == id) {
+      vkUsers.unshift(user);
+    }
+
+    return user.user_id != id;
+  });
+};
+
+function actionOnClick(event, nameOfArray, vkFriendsBlock, myFriendBlock) {
+  if(event.target.classList.contains('item-close') || event.target.classList.contains('fa-times') || event.target.classList.contains('fa-plus')) {
+    console.log('enter in if');
+    let idUser = event.target.parentElement.parentElement.dataset.id;
+
+    if(nameOfArray == 'vk') {
+      setVkUsers(idUser);
+    } else {
+      setMyFriends(idUser);
+    }
+
+    renderUsers(vkUsers, vkFriendsBlock);
+    renderUsers(myFriends, myFriendBlock, 'remove');
+  }
+}
+
 
--- app/js/build.js
+++ app/js/build.js
@@ -4,75 +4,58 @@ VK.init({
   apiId: 5629026
 });
 
-new Promise(function (resolve, reject) {
-  VK.Auth.login(function (response) {
-    if (response.status == 'connected') {
-      resolve();
-    } else {
-      console.log(response);
-    }
-  }, 2 | 4);
-}).then(function () {
-  return new Promise(function (resolve) {
-    VK.api('friends.get', { fields: 'photo_100', order: 'name', name_case: 'nom' }, function (answer) {
-      if (answer.response) {
-        resolve(answer.response);
-      }
-    });
-  });
-}).then(function (usersVk) {
-  var vkUsers = getFromLocalStorage('vkUsers') || usersVk;
-  var myFriends = getFromLocalStorage('myFriends') || [];
+function getFromLocalStorage(key) {
+  return JSON.parse(localStorage[key] || null);
+};
+
+var vkUsers = getFromLocalStorage('vkUsers') || null;
+var myFriends = getFromLocalStorage('myFriends') || [];
+
+window.addEventListener('load', function () {
   var searchInputMyFriend = document.querySelector('.filter-app-search-input__myFriends input');
   var vkFriendsBlock = document.querySelector('.filter-app-friends-your__wrapper');
   var myFriendBlock = document.querySelector('.filter-app-friends-in-list__wrapper');
   var searchInputVkFriends = document.querySelector('.filter-app-search-input input');
   var saveButton = document.querySelector('.filter-app-footer button');
 
-  function getFromLocalStorage(key) {
-    if (localStorage[key]) {
-      return JSON.parse(localStorage[key]);
-    }
-
-    return false;
-  };
-
-  function renderUsers(arrayOfUsers, blockToAppend, action) {
-    blockToAppend.innerHTML = '';
-
-    arrayOfUsers.forEach(function (user) {
-      var userItem = document.createElement('div');
-
-      userItem.setAttribute('draggable', true);
-      userItem.classList.add('item');
-      userItem.setAttribute('data-id', user.user_id);
-      userItem.setAttribute('id', user.user_id);
-
-      if (action == 'remove') {
-        userItem.innerHTML = '\n        <div class="item-logo" style="background: url(' + user.photo_100 + '); background-size: contain; "></div>\n        <h3 class="item-title">' + user.first_name + ' ' + user.last_name + '</h3>\n        <div class="close item-close">\n          <i class="fa fa-times" aria-hidden="true"></i>\n        </div>';
-      } else {
-        userItem.innerHTML = '\n        <div class="item-logo" style="background: url(' + user.photo_100 + '); background-size: contain; "></div>\n        <h3 class="item-title">' + user.first_name + ' ' + user.last_name + '</h3>\n        <div class="close item-close">\n          <i class="fa fa-plus" aria-hidden="true"></i>\n        </div>';
-      }
-
-      userItem.addEventListener('dragstart', function (event) {
-        event.dataTransfer.setData("userItem", event.target.dataset.id);
+  if (!vkUsers) {
+    new Promise(function (resolve, reject) {
+      VK.Auth.login(function (response) {
+        if (response.status == 'connected') {
+          resolve();
+        } else {
+          console.log(response);
+        }
+      }, 2 | 4);
+    }).then(function () {
+      return new Promise(function (resolve) {
+        VK.api('friends.get', { fields: 'photo_100', order: 'name', name_case: 'nom' }, function (answer) {
+          if (answer.response) {
+            vkUsers = answer.response;
+            console.log(vkUsers);
+            resolve();
+          }
+        });
       });
-
-      blockToAppend.appendChild(userItem);
+    }).then(function () {
+      initFilterApp(vkFriendsBlock, myFriendBlock);
     });
+  } else {
+    initFilterApp(vkFriendsBlock, myFriendBlock);
   }
 
-  renderUsers(vkUsers, vkFriendsBlock);
-  renderUsers(myFriends, myFriendBlock, 'remove');
+  vkFriendsBlock.addEventListener('click', function (event) {
+    actionOnClick(event, 'vk', vkFriendsBlock, myFriendBlock);
+  });
+
+  myFriendBlock.addEventListener('click', function (event) {
+    actionOnClick(event, 'myFriend', vkFriendsBlock, myFriendBlock);
+  });
 
   searchInputVkFriends.addEventListener('keyup', function (event) {
     var inputValue = event.currentTarget.value;
     var searchArrayOfUsers = vkUsers.filter(function (user) {
-      if (user.first_name.indexOf(inputValue) != -1) {
-        return true;
-      } else {
-        return false;
-      }
+      return user.first_name.includes(inputValue) == true;
     });
 
     renderUsers(searchArrayOfUsers, vkFriendsBlock);
@@ -81,69 +64,13 @@ new Promise(function (resolve, reject) {
   searchInputMyFriend.addEventListener('keyup', function (event) {
     var inputValue = event.currentTarget.value;
     var searchArrayOfUsers = myFriends.filter(function (user) {
-      if (user.first_name.indexOf(inputValue) != -1) {
-        return true;
-      } else {
-        return false;
-      }
+      return user.first_name.includes(inputValue) == true;
     });
 
     renderUsers(searchArrayOfUsers, myFriendBlock, 'remove');
   });
 
-  vkFriendsBlock.addEventListener('click', function (event) {
-    if (event.target.classList.contains('item-close') || event.target.classList.contains('fa-plus')) {
-      // ToDO Сделать проверку если это крести и если это блок крестика
-      var idUser = event.target.parentElement.parentElement.dataset.id;
-
-      setVkUsers(idUser);
-
-      renderUsers(vkUsers, vkFriendsBlock);
-      renderUsers(myFriends, myFriendBlock, 'remove');
-    }
-  });
-
-  myFriendBlock.addEventListener('click', function (event) {
-    if (event.target.classList.contains('item-close') || event.target.classList.contains('fa-times')) {
-      // ToDO Сделать проверку если это крести и если это блок крестика
-      var idUser = event.target.parentElement.parentElement.dataset.id;
-
-      setMyFriends(idUser);
-      renderUsers(vkUsers, vkFriendsBlock);
-      renderUsers(myFriends, myFriendBlock, 'remove');
-    }
-  });
-
-  function setVkUsers(id) {
-    vkUsers = vkUsers.filter(function (user) {
-      if (user.user_id == id) {
-        myFriends.push(user);
-      }
-
-      if (user.user_id != id) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
-
-  function setMyFriends(id) {
-    myFriends = myFriends.filter(function (user) {
-      if (user.user_id == id) {
-        vkUsers.unshift(user);
-      }
-
-      if (user.user_id != id) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
-
-  // Drop to vkFriendBlock
-
+  // Drag & Drop HTML5 Api
   vkFriendsBlock.addEventListener('dragover', function (event) {
     event.preventDefault();
   });
@@ -157,8 +84,6 @@ new Promise(function (resolve, reject) {
     setMyFriends(data);
     renderUsers(vkUsers, vkFriendsBlock);
   });
-
-  // Drop to myFriendsBlock
 
   myFriendBlock.addEventListener('dragover', function (event) {
     event.preventDefault();
@@ -174,9 +99,79 @@ new Promise(function (resolve, reject) {
     renderUsers(myFriends, myFriendBlock, 'remove');
   });
 
+  // Save button to localStorage
   saveButton.addEventListener('click', function (event) {
     localStorage.setItem('vkUsers', JSON.stringify(vkUsers));
     localStorage.setItem('myFriends', JSON.stringify(myFriends));
   });
 });
+
+function initFilterApp(vkFriendsBlock, myFriendBlock) {
+  renderUsers(vkUsers, vkFriendsBlock);
+  renderUsers(myFriends, myFriendBlock, 'remove');
+}
+
+function renderUsers(arrayOfUsers, blockToAppend, action) {
+  blockToAppend.innerHTML = '';
+
+  arrayOfUsers.forEach(function (user) {
+    var userItem = document.createElement('div');
+    var innerNodes = '<div class="item-logo" style="background: url(' + user.photo_100 + '); background-size: contain; "></div>\n                      <h3 class="item-title">' + user.first_name + ' ' + user.last_name + '</h3>';
+
+    userItem.setAttribute('draggable', true);
+    userItem.classList.add('item');
+    userItem.setAttribute('data-id', user.user_id);
+    userItem.setAttribute('id', user.user_id);
+
+    if (action == 'remove') {
+      innerNodes = innerNodes.concat('\n        <div class="close item-close">\n           <i class="fa fa-times" aria-hidden="true"></i>\n        </div>\n      ');
+    } else {
+      innerNodes = innerNodes.concat('\n        <div class="close item-close">\n           <i class="fa fa-plus" aria-hidden="true"></i>\n        </div>\n      ');
+    };
+
+    userItem.innerHTML = innerNodes;
+
+    userItem.addEventListener('dragstart', function (event) {
+      event.dataTransfer.setData("userItem", event.target.dataset.id);
+    });
+
+    blockToAppend.appendChild(userItem);
+  });
+};
+
+function setVkUsers(id) {
+  vkUsers = vkUsers.filter(function (user) {
+    if (user.user_id == id) {
+      myFriends.push(user);
+    }
+
+    return user.user_id != id;
+  });
+};
+
+function setMyFriends(id) {
+  myFriends = myFriends.filter(function (user) {
+    if (user.user_id == id) {
+      vkUsers.unshift(user);
+    }
+
+    return user.user_id != id;
+  });
+};
+
+function actionOnClick(event, nameOfArray, vkFriendsBlock, myFriendBlock) {
+  if (event.target.classList.contains('item-close') || event.target.classList.contains('fa-times') || event.target.classList.contains('fa-plus')) {
+    console.log('enter in if');
+    var idUser = event.target.parentElement.parentElement.dataset.id;
+
+    if (nameOfArray == 'vk') {
+      setVkUsers(idUser);
+    } else {
+      setMyFriends(idUser);
+    }
+
+    renderUsers(vkUsers, vkFriendsBlock);
+    renderUsers(myFriends, myFriendBlock, 'remove');
+  }
+}
 //# sourceMappingURL=build.js.map
